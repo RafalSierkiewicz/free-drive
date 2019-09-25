2316652c8c08562da4a316fa4050b0fb4b39cd63
Rafal <rafal.ss@interia.pl>
2019-09-25 18:08:35 +0200

    Initial commit

---
+++ src/main/java/com/open/drive/DriveApplication.java
@@ -0,0 +1,13 @@
+package com.open.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveApplication.class, args);
+	}
+
+}
