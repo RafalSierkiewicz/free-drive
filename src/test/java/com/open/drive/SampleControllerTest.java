b949b26445ad30275a75694e34b8dae1d7b93a0d
Rafal <rafal.ss@interia.pl>
2019-09-26 14:35:38 +0200

    Spring tests are stared by CI

--- src/test/java/com/open/drive/SampleControllerTest.java
+++ src/test/java/com/open/drive/SampleControllerTest.java
@@ -3,26 +3,28 @@ package com.open.drive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SampleControllerTest {
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mock;
 
     @Test
-    public void helloShouldReturnDefaultMessage(){
-        String url = "http://localhost:"+port+"/";
-
-        assertThat(this.restTemplate.getForObject(url, String.class)).contains("Hello world");
+    public void helloShouldReturnDefaultMessage() throws Exception {
+        mock.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello world")));
     }
 }
